d6abb78f6661ec4536b4ad5fbec302fcab9ab8bb
Abdelaziz Eroui <kapi.abdel@gmail.com>
2020-03-16 13:22:16 +0000

    Export kinds to constants

--- base.js
+++ base.js
@@ -1,88 +1,103 @@
 // define the objects
 var isPaused = false;
 
-const INFECTED = 0;
-const RECOVERED = 1;
-const DEAD = 2;
+// Cells kinds constants 
+const NORMAL = 0;
+const INFECTED = 1;
+const RECOVERED = 2;
+const DEAD = 3;
+
+const minDeathTime = 150;
+const maxDeathTime = 300;
+const minRecoveryTime = 200;
+const maxRecoveryTime = 300;
 
 function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 
-function Ball(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-    this.stationary = false;
-    this.counter_rec = random(1000, 2000); // random recovery time between 1000 and 2000 frames
-    this.counter_death = random(1500, 2500); // random death time if not recovered time between 1500 and 2500 frames
+function getColor(kind) {
+  switch(kind) {
+    case NORMAL: return 'rgb(' + 100 + ',' + 100 + ',' + 100 +')';
+    case RECOVERED: return 'rgb(' + 0 + ',' + 255 + ',' + 0 +')';
+    case INFECTED: return 'rgb(' + 255 + ',' + 0 + ',' + 0 +')';
+    case DEAD: return 'rgb(' + 0 + ',' + 0 + ',' + 255 +')';
+    default :
+      return 'rgb(' + 100 + ',' + 100 + ',' + 100 +')';
+  }
+}
+
+function Ball(x, y, velX, velY, size, kind) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.size = size;
+  this.kind = kind;
+  // random recovery time between 1000 and 2000 frames
+  this.counter_rec = random(minDeathTime, maxDeathTime); 
+  // random death time if not recovered time between 1500 and 2500 frames
+  this.counter_death = random(minDeathTime, maxRecoveryTime); 
 }
 
 Ball.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+  ctx.beginPath();
+  ctx.fillStyle = getColor(this.kind);
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
 Ball.prototype.update = function() {
-    if ((this.x + this.size) >= width) {
-      this.velX = -(this.velX);
-    }
-  
-    if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
-    }
-  
-    if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
-    }
-  
-    if ((this.y - this.size) <= 0) {
-      this.velY = -(this.velY);
-    }
-    
-    if (!this.stationary) {
-        this.x += this.velX;
-        this.y += this.velY;
-        this.velX = random(-10,10);
-        this.velY = random(-10,10);
-    }
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
 
-    if (this.counter_rec > 0 && this.color === 'rgb(' + 255 + ',' + 0 + ',' + 0 +')') {
-        this.counter_rec--;
-    }
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
 
-    if (this.counter_death > 0 && this.color === 'rgb(' + 255 + ',' + 0 + ',' + 0 +')') {
-        this.counter_death--;
-    }
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
 
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+  
+  if (this.kind !== DEAD) {
+      this.x += this.velX;
+      this.y += this.velY;
+      this.velX = random(-10,10);
+      this.velY = random(-10,10);
+  }
+
+  if (this.kind == INFECTED && this.counter_rec > 0) {
+    --this.counter_rec;
     if (this.counter_rec <= 0) {
-        this.color = 'rgb(' + 0 + ',' + 255 + ',' + 0 +')';
+      this.kind = RECOVERED;
     }
-
+  } else if (this.kind == INFECTED && this.counter_death > 0) {
+    this.counter_death--;
     if (this.counter_death <= 0) {
-        this.color = 'rgb(' + 0 + ',' + 0 + ',' + 255 +')';
-        this.stationary = true;
+      this.kind = DEAD;
     }
+  }
 }
 
 Ball.prototype.collisionDetect = function() {
-    for (let j = 0; j < balls.length; j++) {
-      if (!(this === balls[j])) {
-        const dx = this.x - balls[j].x;
-        const dy = this.y - balls[j].y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
-  
-        if (distance < this.size + balls[j].size && balls[j].color === 'rgb(' + 255 + ',' + 0 + ',' + 0 +')' && this.color != 'rgb(' + 0 + ',' + 255 + ',' + 0 +')') {
-          balls[j].color = this.color = 'rgb(' + 255 + ',' + 0 + ',' + 0 +')';
-        }
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size 
+        && balls[j].kind === INFECTED && this.kind != RECOVERED) {
+        this.kind = INFECTED;
       }
     }
+  }
 }
 
 
@@ -103,19 +118,20 @@ let balls = [];
 while (balls.length < pop_size) {
   let size = 4;
   let ball = new Ball(
-    
     random(0 + size,width - size),
     random(0 + size,height - size),
     random(-10,10),
     random(-10,10),
-    'rgb(' + 100 + ',' + 100 + ',' + 100 +')',
-    size
+    size,
+    NORMAL
   );
 
   balls.push(ball);
 }
 
-balls[random(0, pop_size)].color = 'rgb(' + 255 + ',' + 0 + ',' + 0 +')'; //random sick person
+// Random sick Person
+balls[random(0, pop_size)].kind = INFECTED;
+
 
 // for (let i = 0; i < balls.length; i++) {
 //     if (i%4 !== 0){
@@ -136,7 +152,7 @@ function loop() {
   if (!isPaused) {
     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
     ctx.fillRect(0, 0, width, height);
-  
+
     for (let i = 0; i < balls.length; i++) {
       balls[i].draw();
       balls[i].update();
